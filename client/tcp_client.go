b7b433b65d39baeaea3fa1aff192d744c8ecfec4
imthaghost <imthaghost@protonmail.com>
2020-02-29 13:32:06 -0800

    update tcp server

---
+++ client/tcp_client.go
@@ -0,0 +1,79 @@
+package client
+
+import (
+	"io"
+	"log"
+	"net"
+
+	"github.com/imthaghost/chatserv/protocol"
+)
+
+type TcpChatClient struct {
+	conn      net.Conn
+	cmdReader *protocol.CommandReader
+	cmdWriter *protocol.CommandWriter
+	name      string
+	incoming  chan protocol.MessageCommand
+}
+
+func NewClient() *TcpChatClient {
+	return &TcpChatClient{
+		incoming: make(chan protocol.MessageCommand),
+	}
+}
+
+func (c *TcpChatClient) Dial(address string) error {
+	conn, err := net.Dial("tcp", address)
+
+	if err == nil {
+		c.conn = conn
+	}
+
+	c.cmdReader = protocol.NewCommandReader(conn)
+	c.cmdWriter = protocol.NewCommandWriter(conn)
+
+	return err
+}
+
+func (c *TcpChatClient) Start() {
+	for {
+		cmd, err := c.cmdReader.Read()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Printf("Read error %v", err)
+		}
+
+		if cmd != nil {
+			switch v := cmd.(type) {
+			case protocol.MessageCommand:
+				c.incoming <- v
+			default:
+				log.Printf("Unknown command: %v", v)
+			}
+		}
+	}
+}
+
+func (c *TcpChatClient) Close() {
+	c.conn.Close()
+}
+
+func (c *TcpChatClient) Incoming() chan protocol.MessageCommand {
+	return c.incoming
+}
+
+func (c *TcpChatClient) Send(command interface{}) error {
+	return c.cmdWriter.Write(command)
+}
+
+func (c *TcpChatClient) SetName(name string) error {
+	return c.Send(protocol.NameCommand{name})
+}
+
+func (c *TcpChatClient) SendMessage(message string) error {
+	return c.Send(protocol.SendCommand{
+		Message: message,
+	})
+}

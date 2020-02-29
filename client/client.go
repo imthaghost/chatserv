7550e4a4b81b3148efc99441277cb12fbaf13147
imthaghost <imthaghost@protonmail.com>
2020-02-29 13:30:35 -0800

    update Client functionality

--- client/client.go
+++ client/client.go
@@ -1,7 +1,10 @@
 package client
 
-import "github.com/imthaghost/gochat/protocol"
+import "github.com/imthaghost/chatserv/protocol"
 
+type messageHandler func(string)
+
+// ChatClient ....
 type ChatClient interface {
 	Dial(address string) error
 	Start()
@@ -9,6 +12,5 @@ type ChatClient interface {
 	Send(command interface{}) error
 	SetName(name string) error
 	SendMessage(message string) error
-	Error() chan error
 	Incoming() chan protocol.MessageCommand
 }
